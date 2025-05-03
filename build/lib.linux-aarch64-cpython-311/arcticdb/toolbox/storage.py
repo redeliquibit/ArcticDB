b22a201c9f277b404822baeba0aa49784358e79a
Ubuntu <ubuntu@ip-172-31-9-50.ap-northeast-1.compute.internal>
2025-05-03 20:31:09 +0000

    revert gitignore

--- build/lib.linux-aarch64-cpython-311/arcticdb/toolbox/storage.py
+++
@@ -1,3 +0,0 @@
-from typing import NamedTuple, Union, List
-
-SymbolVersionsPair = NamedTuple("SymbolVersionsPair", [("id", Union[int, str]), ("versions", List[Union[int, str]])])
--- build/lib.linux-aarch64-cpython-311/arcticdb/version_store/__init__.py
+++
@@ -1,2 +0,0 @@
-from arcticdb.version_store._common import TimeFrame
-from arcticdb.version_store._store import NativeVersionStore, VersionedItem
